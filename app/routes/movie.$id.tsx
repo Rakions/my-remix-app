c61ed11beab2c5fe02e299d98ea13e24090811a1
Rakions <ogmohammed2004@hotmail.com>
2023-12-01 16:19:50 +0100

    fixed token

--- app/routes/movie.$id.tsx
+++ app/routes/movie.$id.tsx
@@ -5,8 +5,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
   const url = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?language=en-US`, {
     headers: {
       accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMjY4NTA1ZTQyMzZmNTliNmQ4NTEyZGYyOGRkMGNkYSIsInN1YiI6IjY1NjkyMWFiNzFmMDk1MDBmZWI3ODQ5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iK-9Z2z_r1spf5OTztdc7hZ3E_vQUDOz-ydMGN3DLHI",
+      Authorization: `Bearer ${process.env.DATABASE_URL}`,
     },
   });
 
